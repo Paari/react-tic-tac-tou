a18cfd5726e945ef082b37aaf7f6eb63261ea345
Paari <0p0a0r0i@gmail.com>
2017-08-23 00:48:44 +0530

    Mark boxes as 0 or X

--- src/Components/Block/index.js
+++ src/Components/Block/index.js
@@ -4,13 +4,28 @@ class Block extends Component {
   constructor() {
     super();
     this.state = {
-      status: false
+      marked: false
     }
+
+    // bind the click function to this
+    this.markTheBox = this.markTheBox.bind(this);
+  }
+
+  // mark the box with the active symbol: 0 or X
+  markTheBox() {
+    const {mark, index, updateTheBox} = this.props;
+
+    // call the function from the props and pass the values
+    updateTheBox(index, mark);
   }
 
   render() {
     return (
-      <div className="block__item" />
+      <div onClick={this.markTheBox} className="block__item">
+        <span className="block__item--mark">
+          {this.props.mark}
+        </span>
+      </div>
     )
   }
 }
--- src/Components/index.js
+++ src/Components/index.js
@@ -6,20 +6,47 @@ class Wrapper extends Component {
     super();
     this.state = {
       activePlayer: 'X',
+      boxes: ['','','','','','','','','']
     }
+
+    this.updateTheBox = this.updateTheBox.bind(this);
   }
+
+  // update the box on click based on the index
+  updateTheBox (index, marker) {
+    if (marker != '') return false;
+    // update the value with active player
+    let updatedBox = this.state.boxes;
+    updatedBox[index] = this.state.activePlayer;
+
+    // flip the marker of the player 0 to X or X to 0
+    let activeMarker = '0'
+    if (this.state.activePlayer === activeMarker) {
+      activeMarker = 'X'
+    }
+
+    // update the state
+    this.setState({
+      boxes: updatedBox,
+      activePlayer: activeMarker
+    })
+  }
+
+
   render() {
     return (
       <div className="main-wrapper">
-      <Block />
-      <Block />
-      <Block />
-      <Block />
-      <Block />
-      <Block />
-      <Block />
-      <Block />
-      <Block />
+        {this.state.boxes.map((marker, index) => {
+          return (
+            <Block
+            key={index}
+            index={index}
+            mark={marker}
+            active={this.state.activePlayer}
+            updateTheBox={this.updateTheBox}
+            />
+          );
+        })}
       </div>
     )
   }
