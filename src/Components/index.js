8802844b0aa25bd9068f5e3d8e83f5fe6a07e51b
paritosh_pundir <0p0a0r0i@gmail.com>
2017-08-23 23:50:22 +0530

    game logic: if there are same elements in the vertical line.

--- src/Components/index.js
+++ src/Components/index.js
@@ -12,13 +12,34 @@ class Wrapper extends Component {
     this.updateTheBox = this.updateTheBox.bind(this);
   }
 
+  // main game logic
+  checkResult (index) {
+    const boxes = this.state.boxes;
+
+    // if it's the second row move the check to line one
+    if (index > 2 && index < 6) {
+      index -= 3;
+    }
+
+    // similarly if it is line third line
+    if (index > 5) {
+      index -= 6;
+    }
+
+    // now check if vertical markers are same
+    if (boxes[index] === boxes[index + 3] && boxes[index + 3] === boxes[index + 6]) {
+      return true;
+    }
+    return false;
+  }
+
   // update the box on click based on the index
   updateTheBox (index, marker) {
-    if (marker != '') return false;
+    if (marker !== '') return false;
     // update the value with active player
     let updatedBox = this.state.boxes;
     updatedBox[index] = this.state.activePlayer;
-
+    console.log(this.checkResult(index));
     // flip the marker of the player 0 to X or X to 0
     let activeMarker = '0'
     if (this.state.activePlayer === activeMarker) {
